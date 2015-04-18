c0f2b72d7d2750e32e325104696ec42fa2cad1e7
filipealva <ofilipealvarenga@gmail.com>
2015-04-18 02:26:07 -0300

    Create DotView

---
+++ Filipe Alvarenga/View/Dot.swift
@@ -0,0 +1,21 @@
+//
+//  Dot.swift
+//  Filipe Alvarenga
+//
+//  Created by Filipe Alvarenga on 17/04/15.
+//  Copyright (c) 2015 Filipe Alvarenga. All rights reserved.
+//
+
+import UIKit
+
+class Dot: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
